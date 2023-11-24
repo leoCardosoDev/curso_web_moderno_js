3f5caada61c182660d89f136470552338b604582
leoCardosoDev <leocardosodev@gmail.com>
2023-11-24 07:07:14 -0300

    feat: add tipagem

---
+++ 01_javascript/01_fundamentos/03_tipagem_fraca.js
@@ -0,0 +1,16 @@
+let qualquer = 'É uma string'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 1
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evite nomes genérios
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso... 
